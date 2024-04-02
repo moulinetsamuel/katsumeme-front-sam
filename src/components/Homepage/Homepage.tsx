49cfcfb06cbcf936d3076545eb3d414c4293c002
SebastienMaunoir <sebastien.maunoir@oclock.school>
2024-04-02 17:59:04 +0200

    ✨feature: Homepage and MemeCard creation + Header customization

---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,17 @@
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+import MemeCard from '../MemeCard/MemeCard';
+
+import './Homepage.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <MemeCard />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
