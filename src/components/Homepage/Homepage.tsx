e854bd06aa0b0737473495a575d68f292732a645
ErbilginSimin <simin.erbilgin@oclock.school>
2024-04-11 13:49:58 +0200

    ✨feature: create-meme route created on button create

--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,23 +1,29 @@
-import { BrowserRouter, Link } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import MemePage from '../MemePage/MemePage';
 import './Homepage.scss';
 import MemeEditor from '../CreateMemePage/MemeEditor';
+import CreateMemePage from '../CreateMemePage/CreateMemePage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app">
-        <div className="Header">
-          <Header />
-        </div>
-        <MemePage />
-        <Footer />
-        <MemeEditor />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create-meme" element={<CreateMemePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <MemePage />
+      <Footer />
+    </>
   );
 }
 
