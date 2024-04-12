7f22d30c737d1b56ce6d08d765969921c2287d85
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-12 09:40:58 +0200

    ✨feature: Refactor MemePage component to remove unused code

--- src/components/MemePage/MemePage.tsx
+++ src/components/MemePage/MemePage.tsx
@@ -5,17 +5,6 @@ import './MemePage.scss';
 import ButtonCreate from '../ButtonCreate/ButtonCreate';
 
 function MemePage() {
-  const handleButton = async () => {
-    await axios
-      .get('https://katsumeme-8c128449f9bf.herokuapp.com/api/test')
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-
   return (
     <div>
       <div className="ButtonContainer">
