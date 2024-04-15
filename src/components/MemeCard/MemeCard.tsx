57dbeeb5babb4e0e279ce7e81739f5b454afe10a
GwendalLourdel <gwendal.lourdel@oclock.school>
2024-04-15 13:34:54 +0200

    ✨feature: init dynamisation

--- src/components/MemeCard/MemeCard.tsx
+++ src/components/MemeCard/MemeCard.tsx
@@ -8,98 +8,94 @@ import './MemeCard.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button } from 'react-bootstrap';
 import { useEffect, useState } from 'react';
+import axiosInstance from '../API/axios';
+import AuthProvider from '../Auth/AuthProvider';
+
+interface Meme {
+  id: number;
+  image_url: string;
+  title: string;
+  created_at: string;
+  tags: { tags: { name: string } }[];
+  author: { nickname: string; avatar_url: string };
+  _count: { liked_by: number };
+  dislikeCount: { liked_by: number };
+  isBookmarked: boolean;
+  isliked: boolean;
+}
 
 function MemeCard() {
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
-    return localStorage.getItem('token') !== null;
-  }); // State to track if user is login or not
+  const [meme, setMeme] = useState<meme[]>([]);
 
   useEffect(() => {
-    const handleStorageCHange = () => {
-      setIsLoggedIn(localStorage.getItem('token') !== null);
+    const fetchMeme = async () => {
+      try {
+        const response = await axiosInstance.get('/api/memes?limit=6&page=1');
+        setMeme(response.data);
+      } catch (error) {
+        console.error('Error fetching meme data', error);
+      }
     };
-    window.addEventListener('storage', handleStorageCHange);
 
-    return () => {
-      window.removeEventListener('storage', handleStorageCHange);
-    };
+    fetchMeme();
+    // const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
+    //   return localStorage.getItem('token') !== null;
+    // }); // State to track if user is login or not
+
+    // useEffect(() => {
+    //   const handleStorageCHange = () => {
+    //     setIsLoggedIn(localStorage.getItem('token') !== null);
+    //   };
+    //   window.addEventListener('storage', handleStorageCHange);
+
+    //   return () => {
+    //     window.removeEventListener('storage', handleStorageCHange);
+    //   };
   }, []);
 
   return (
     <div className="MemeCardContainer">
-      <Card className="CardStyle" style={{ width: '25rem' }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle>Author created at ...</Card.Subtitle>
+      {meme.map((meme, index) => (
+        <Card key={index} className="CardStyle" style={{ width: '25rem' }}>
+          <Card.Body>
+            <Card.Title>{meme.title}</Card.Title>
+            <Card.Subtitle>{`Auteur: ${meme.author.nickname}, créé le ${meme.created_at}`}</Card.Subtitle>
 
-          <Card.Img variant="top" src="/favicon.ico" />
-          <section>
-            <BsTags />
-            <div>
-              <Card.Link href="#">TAGS</Card.Link>
-              <Card.Link href="#">TAGS</Card.Link>
-              <Card.Link href="#">TAGS</Card.Link>
-            </div>
-          </section>
+            <Card.Img variant="top" src={meme.image_url} />
+            <section>
+              <BsTags />
+              <div>
+                {meme.tags.map((tag, tagIndex) => (
+                  <Card.Link key={tagIndex} href="#">
+                    {tag.tags.name}
+                  </Card.Link>
+                ))}
+              </div>
+            </section>
 
-          <div className="Emotes">
-            <Button variant="primary">
-              <FaComment />
-            </Button>
-            <Button variant="primary">
-              <AiFillLike />
-            </Button>
-            <Button variant="primary">
-              <AiFillDislike />
-            </Button>
-            <Button variant="primary">
-              <FaDownload />
-            </Button>
-            {isLoggedIn && (
+            <div className="Emotes">
               <Button variant="primary">
-                <MdOutlineStarBorder />
-                {/* <MdOutlineStar /> */}
+                <FaComment />
               </Button>
-            )}
-          </div>
-        </Card.Body>
-      </Card>
-
-      <Card className="CardStyle" style={{ width: '25rem' }}>
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Subtitle>Author created at ...</Card.Subtitle>
-
-          <Card.Img variant="top" src="src/assets/Logo slogan noir.png" />
-          <section>
-            <BsTags />
-            <div>
-              <Card.Link href="#">TAGS</Card.Link>
-              <Card.Link href="#">TAGS</Card.Link>
-              <Card.Link href="#">TAGS</Card.Link>
+              <Button variant="primary">
+                <AiFillLike />
+              </Button>
+              <Button variant="primary">
+                <AiFillDislike />
+              </Button>
+              <Button variant="primary">
+                <FaDownload />
+              </Button>
+              {isLoggedIn && (
+                <Button variant="primary">
+                  <MdOutlineStarBorder />
+                  {/* <MdOutlineStar /> */}
+                </Button>
+              )}
             </div>
-          </section>
-
-          <div className="Emotes">
-            <Button variant="primary">
-              <FaComment />
-            </Button>
-            <Button variant="primary">
-              <AiFillLike />
-            </Button>
-            <Button variant="primary">
-              <AiFillDislike />
-            </Button>
-            <Button variant="primary">
-              <FaDownload />
-            </Button>
-            <Button variant="primary">
-              <MdOutlineStarBorder />
-              {/* <MdOutlineStar /> */}
-            </Button>
-          </div>
-        </Card.Body>
-      </Card>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 }
