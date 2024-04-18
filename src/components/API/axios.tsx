6d9548531ff0a6fe1ba87e0db0a30505a5d09c4f
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-18 21:41:03 +0200

    Refactor axios.tsx to handle access token and refresh token errors

--- src/components/API/axios.tsx
+++ src/components/API/axios.tsx
@@ -2,10 +2,28 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization:
-      'Bearer ' + JSON.parse(localStorage.getItem('accessToken') || '{}'),
-  },
 });
+
+function getAccessToken() {
+  const tokensString = localStorage.getItem('tokens');
+  if (tokensString) {
+    const tokens = JSON.parse(tokensString);
+    return tokens.accessToken;
+  }
+  return null;
+}
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
