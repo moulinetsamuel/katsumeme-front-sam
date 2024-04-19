258f6ebf6e1eee6a6332286f57289dadebeb6edc
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-19 16:59:34 +0200

    ✨feature: Refactor axios interceptors to handle 401 responses and refresh access token

--- src/components/UserStore/UserState.tsx
+++ src/components/UserStore/UserState.tsx
@@ -9,15 +9,41 @@ interface UserState {
   setAppState: () => void;
   uploadCount: number;
   incrementUploadCount: () => void;
+  refreshAccessToken: () => void;
 }
 
 const useUserStore = create<UserState>((set) => ({
   user: {},
+
   isAuthenticated: false,
+
   error: null,
+
   uploadCount: 0,
+
   incrementUploadCount: () =>
     set((state) => ({ uploadCount: state.uploadCount + 1 })),
+
+  refreshAccessToken: async () => {
+    set({ error: null });
+
+    try {
+      const responseRefresh = await axiosInstance.post('/api/auth/refresh', {
+        refreshToken: getRefreshToken(),
+      });
+      localStorage.setItem(
+        'tokens',
+        JSON.stringify({
+          accessToken: responseRefresh?.data?.accessToken,
+          refreshToken: responseRefresh?.data?.refreshToken,
+        })
+      );
+      await retryAfterRefresh(set);
+    } catch (error) {
+      handleOtherErrors(set, error);
+    }
+  },
+
   setAppState: async () => {
     set({ error: null });
 
@@ -26,7 +52,7 @@ const useUserStore = create<UserState>((set) => ({
       set({ user: response.data, isAuthenticated: true });
     } catch (error) {
       if ((error as AxiosError).response?.status === 401) {
-        await refreshAccessToken(set);
+        useUserStore.getState().refreshAccessToken();
       } else {
         handleOtherErrors(set, error);
       }
@@ -41,24 +67,6 @@ function getRefreshToken() {
     return tokens.refreshToken;
   }
   return null;
-}
-
-async function refreshAccessToken(set: (state: Partial<UserState>) => void) {
-  try {
-    const responseRefresh = await axiosInstance.post('/api/auth/refresh', {
-      refreshToken: getRefreshToken(),
-    });
-    localStorage.setItem(
-      'tokens',
-      JSON.stringify({
-        accessToken: responseRefresh?.data?.accessToken,
-        refreshToken: responseRefresh?.data?.refreshToken,
-      })
-    );
-    await retryAfterRefresh(set);
-  } catch (error) {
-    handleOtherErrors(set, error);
-  }
 }
 
 async function retryAfterRefresh(set: (state: Partial<UserState>) => void) {
