c99e99c4a7b0e9f1d9ecd61f151fde64f9d0bcd4
moulinetsamuel <samuel.moulinet@oclock.school>
2024-04-23 00:57:53 +0200

    Refactor DeleteMeme component to use correct API endpoint

--- src/components/MemeCard/DeleteMeme.tsx
+++ src/components/MemeCard/DeleteMeme.tsx
@@ -1,7 +1,6 @@
 import Button from 'react-bootstrap/Button';
 import axiosInstance from '../API/axios';
 import { TiDelete } from 'react-icons/ti';
-import useUserStore from '../UserStore/UserState';
 
 interface DeleteMemeProps {
   memeId: number;
@@ -10,7 +9,7 @@ interface DeleteMemeProps {
 function DeleteMeme({ memeId }: DeleteMemeProps) {
   const deleteMeme = async () => {
     try {
-      await axiosInstance.delete(`api/meme/${memeId}`);
+      await axiosInstance.delete(`api/memes/${memeId}`);
     } catch (error) {
       console.error('Erreur lors de la suppression du meme', error);
     }
