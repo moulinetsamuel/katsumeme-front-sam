a4de83313d9ebde40decfeecced8faed41d708b7
ErbilginSimin <simin.erbilgin@oclock.school>
2024-05-13 21:22:37 +0200

    header and menu test

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,10 @@
+import { Nav, Navbar, Stack } from 'react-bootstrap';
+import './Header.scss';
 import LoginLogout from '../Button/ButtonLogin-Logout/ButtonLogin-Logout';
 import { Link } from 'react-router-dom';
-import './Header.scss';
+import ButtonShare from '../Button/ButtonShare/ButtonShare';
+import ButtonCreate from '../Button/ButtonCreate/ButtonCreate';
+import { FaBars } from 'react-icons/fa6';
 
 function Header() {
   return (
@@ -13,10 +17,31 @@ function Header() {
             alt="Logo Katsumeme Chat"
           />
         </Link>
-        <div className="Login">
+        <div className="Login ms-auto d-none d-md-block">
           <LoginLogout />
         </div>
       </div>
+
+      {/* Side bar for small screen */}
+      <Navbar expand="md" className="d-md-none ms-auto">
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          className="bg-light"
+        >
+          <FaBars />
+        </Navbar.Toggle>
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="flex-column">
+            <Nav.Link>Katsumeme du moment</Nav.Link>
+            <Nav.Link>Les plus likés</Nav.Link>
+            <Nav.Link>Les moins likés</Nav.Link>
+            <Nav.Link>Derniers memes ajoutés</Nav.Link>
+          </Nav>
+          <div>
+            <LoginLogout />
+          </div>
+        </Navbar.Collapse>
+      </Navbar>
     </header>
   );
 }
